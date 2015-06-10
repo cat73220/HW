be0b17c245ef78440a1c3083560e4c64285c66f8
M.Watanabe <nabe@logvox.jp>
2015-06-10 11:37:15 +0900

    pending using rexml and forcely set utf-8 too

--- mocom/mocom.rb
+++ mocom/mocom.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 # -*- coding: utf-8 -*-
+# coding: utf-8
 
 #
 # monitoring selling comics monthly
@@ -8,42 +9,12 @@
 # for opening URL
 require 'open-uri'
 
-page = open("http://www.bookservice.jp/layout/bs/common/html/schedule/comic_top.html").read
-p page.to_s
+page = open("http://www.bookservice.jp/layout/bs/common/html/schedule/comic_top.html", "r:utf-8").read
 
-=begin
-#
-# rexml streamparseは読み込むhtmlの途中にエラーがあった時の例外処理が複雑になりそうな気配がある。
-# なので、調査停止
-#
+page.each_line { |line|
+  line.chomp!
+  p line
+}
 
-# for parsing html
-require 'rexml/parsers/baseparser'
-require 'rexml/parsers/streamparser'
-require 'rexml/streamlistener'
 
-class Listener
-  include REXML::StreamListener
 
-  def initialize
-    @events = []
-  end
-
-  def text(text)
-    @events << "text[#{text}]"
-  end
-
-  def tag_start(name, attrs)
-    @events << "tag_start[#{name}]"
-  end
-
-  attr_reader :events
-end
-
-listner = Listener.new
-
-page = open("http://www.bookservice.jp/layout/bs/common/html/schedule/comic_top.html").read
-
-REXML::Parsers::StreamParser.new( page.to_s, listner ).parse
-listner.events
-=end
